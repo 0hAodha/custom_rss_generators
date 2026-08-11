660c4245112979005f00400f4a3b46c87059547f
Aindréas Ó hAodha <aindreas@ohaodha.ie>
2026-08-11 20:22:48 +0000

    chore(roisin_dubh_listings.py): use env python shebang

--- src/roisin_dubh_listings.py
+++ src/roisin_dubh_listings.py
@@ -1,4 +1,4 @@
-#!/bin/python3
+#!/usr/bin/env python3
 # Script to consume the event listings JSON feed for the Róisín Dubh pub in Galway and generate an RSS feed
 # The page does appear to offer an RSS feed but it's broken and returns dates in the far future and distant past
 
